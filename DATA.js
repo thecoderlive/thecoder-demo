b0c0c17f87447d93559f9eac569152c3077dfd00
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-29 12:57:22 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,84 @@
+export const item = {
+    "id": "c65559a9-1829-416f-84be-44c6a1ed74b9",
+    "trending_songs": [
+        {
+            "id": "7f702339-049a-40c8-a41d-6f95b8ae7657",
+            "trending_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+        },
+        {
+            "id": "787a0025-a31f-4f29-899a-c18d3955208b",
+            "trending_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+        },
+        {
+            "id": "f2210a16-c509-4514-b0ca-2f5474341aa9",
+            "trending_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+        },
+        {
+            "id": "0deb1446-4c67-4ef8-a819-3a2c63b6fac1",
+            "trending_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+        },
+        {
+            "id": "2737ee94-facf-4f53-a349-85cd1bde8674",
+            "trending_song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+        }
+    ],
+    "new_songs": [
+        {
+            "id": "9aa24c4f-ae02-4503-a0ca-f6bc4fb45aa8",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png",
+            "song_name": "medical",
+            "singer": "each"
+        },
+        {
+            "id": "dfa1c9f0-a090-4975-941f-4ce02c6b8468",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png",
+            "song_name": "sense",
+            "singer": "establish"
+        },
+        {
+            "id": "5e2e3135-5d51-458c-b773-1b1a90f3d0a1",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png",
+            "song_name": "society",
+            "singer": "sister"
+        },
+        {
+            "id": "ad900602-83a7-4a32-8a90-9caeaca6ebcf",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png",
+            "song_name": "follow",
+            "singer": "fund"
+        },
+        {
+            "id": "ef8b702d-d692-4bae-84bc-b814d2f789f1",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png",
+            "song_name": "follow",
+            "singer": "wide"
+        }
+    ],
+    "suggested_artists": [
+        {
+            "id": "64e2ef22-6b51-4133-be46-26e76a06224c",
+            "artist_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png",
+            "artist_name": "civil"
+        },
+        {
+            "id": "5b849189-ad49-4227-b04e-3c72869f6a9b",
+            "artist_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png",
+            "artist_name": "picture"
+        },
+        {
+            "id": "2e87a1a6-aefd-427c-9ea9-c799c23d77bb",
+            "artist_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png",
+            "artist_name": "rest"
+        },
+        {
+            "id": "06eac072-c71c-41fc-b34c-6a44cbeee2c9",
+            "artist_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png",
+            "artist_name": "clearly"
+        },
+        {
+            "id": "37932358-6599-4bb2-a23b-70ffbd446f9b",
+            "artist_profile_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png",
+            "artist_name": "yet"
+        }
+    ]
+}
